4e5d6c2e4d85aef7f055e310a0615b8748d653e2
Ankur Mittal <AnkurMittal@Etechaces.com>
2018-04-06 14:50:17 +0530

    initial commit for testing purpose pom file still needs modification

---
+++ src/test/java/com/qa/test/DemoTest2.java
@@ -0,0 +1,44 @@
+package com.qa.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest2 {
+
+	
+	@Test
+	public void Sum()
+	{
+		System.out.println("SUM");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(30, a+b);
+	}
+	
+	@Test
+	public void Sub()
+	{
+		System.out.println("SUB");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(10, b-a);
+	}
+	@Test
+	public void Mul()
+	{
+		System.out.println("Mul");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(200, a*b);
+	}
+	@Test
+	public void Div()
+	{
+		System.out.println("DIV");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(2, b/a);
+	}
+}
